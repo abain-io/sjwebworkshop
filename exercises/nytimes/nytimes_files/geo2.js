ed8c417c71bf2fea9bf9e49478769fe5425b10c0
Scott Jehl <scottjehl@users.noreply.github.com>
2019-05-03 16:29:08 +0700

    Add files via upload

---
+++ exercises/nytimes/nytimes_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "PHNOMPENH",
+      'continent': "AS",
+      'country': "KH",
+      'region': ""
+    },
+    'ip':"116.212.158.30"
+  }]);
+})
+//
+()
+
+;
